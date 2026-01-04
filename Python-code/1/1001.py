dd810bd5a5ad8661b0d348cff28e690e5d38c810
pgsz <962797832@qq.com>
2026-01-04 17:23:13 +0800

    add python-code 文件

---
+++ Python-code/1/1001.py
@@ -0,0 +1,27 @@
+print('hello Python')
+
+# print(2 * 3)
+
+# input("请输入指令: \n")
+
+# var1 = input("输入第一行内容：\n")
+# var2 = input("输入第二行内容：\n")
+# print(var1)
+# print(var2)
+
+
+# 跟 JavaScript 不一样，没有变量提升的说法
+x = 123
+print(x) # 123 
+
+x = 456
+print(x) # 456  
+
+z = y = x
+print(z) # 456
+print(y) # 456
+
+# 多个赋值
+a,b = 1,2
+print(a) # 1
+print(b) # 2
